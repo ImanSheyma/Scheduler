ea02aa33a8f0a8245aa9f72430358acf76bf770a
Iman Sheyma <imansheyma1@gmail.com>
2024-04-15 20:29:50 +0300

    date range fixed in TaskRepository

--- src/main/java/com/byport/scheduler/DAO/TaskRepository.java
+++ src/main/java/com/byport/scheduler/DAO/TaskRepository.java
@@ -10,9 +10,9 @@ public interface TaskRepository extends JpaRepository<Task, Integer>{
     @Query("SELECT t FROM Task t WHERE t.employee.id = :employeeId")
     List<Task> findAllByEmployee(Integer employeeId);
 
-    @Query("SELECT t FROM Task t WHERE t.employee.id = :employeeId AND (t.start >= :start AND t.start <= :end OR t.end >= :start AND t.end <= :end)")
+    @Query("SELECT t FROM Task t WHERE t.employee.id = :employeeId AND (t.start >= :start AND t.start <= :end OR t.end >= :start AND t.end <= :end OR t.start < :start AND t.end > :end)")
     List<Task> findTasksByDateRange(Integer employeeId, Date start, Date end);
 
-    @Query("SELECT t FROM Task t WHERE (t.start >= :start AND t.start <= :end OR t.end >= :start AND t.end <= :end)")
+    @Query("SELECT t FROM Task t WHERE (t.start >= :start AND t.start <= :end OR t.end >= :start AND t.end <= :end OR t.start < :start AND t.end > :end)")
     List<Task> findAllTasksByDateRange(Date start, Date end);
 }
